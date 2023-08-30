ca917e926b873accb2d5908871edb23651d85fe9
yberstro <alfianzikri140@gmail.com>
2023-08-30 22:50:58 +0700

    Meng-aplikasikan Blade Template yield & section

--- resources/views/users/siswa/pengumuman.blade.php
+++ resources/views/users/siswa/pengumuman.blade.php
@@ -1,90 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Pengumuman</title>
+@extends('layouts.template')
+@section('Judul','PENGUMUMAN')
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('../../css/fontawesome-free/css/all.min.css')}}">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('../../css/adminlte.min.css')}}">
-</head>
+@section('Menu')
 
-<body class="hold-transition sidebar-mini">
-<!-- Site wrapper -->
-<div class="wrapper">
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="/" class="nav-link"><strong>SMAN 12 BANDA ACEH</strong></a>
-      </li>
-      
-    </ul>
+  @parent
+    <li class="nav-item">
+      <a href="/pengumuman" class="nav-link active bg-white">
+          <i class="nav-icon fas fa-bullhorn"></i>
+          <p>
+            Pengumuman
+          </p>
+      </a>
+    </li>
 
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
+@stop
 
-  <li class="nav-item d-none d-sm-inline-block">
-    <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault();
-    document.getElementById('logout-form').submit();">Logout</a>
-    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-      @csrf
-  </form>
-  </li>
 
-    </ul>
-  </nav>
-  
-  <!-- /.navbar -->
-
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="imgs/user8-128x128.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">
-            {{ Auth::user()->nm_siswa }}
-          </a>
-        </div>
-      </div>
-
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-
-          <li class="nav-item">
-            <a href="/pengumuman" class="nav-link">
-              <i class="nav-icon fas fa-users"></i>
-              <p>
-                Pengumuman
-              </p>
-            </a>
-          </li>
-
-        </ul>
-      </nav>
-    </div>
-  </aside>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <section class="content-header">
-    </section>
+@section('Konten')
 
     <!-- Main content -->
     <section class="content">
@@ -140,21 +72,4 @@
 
     </section>
     <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-
- 
-
-  <!-- Control Sidebar -->
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
-
-<!-- jQuery -->
-<script src="{{ asset('../../plugins/jquery/jquery.min.js')}}"></script>
-<!-- Bootstrap 4 -->
-<script src="{{ asset('../../js/bootstrap.bundle.min.js')}}"></script>
-<!-- AdminLTE App -->
-<script src="{{ asset('../../js/adminlte.min.js')}}"></script>
-</body>
-</html>
+@stop
